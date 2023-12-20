bba6e63f36f62f822c0cb72e767c286612900d41
Indrajeet <indrajeetkumarnayak3@gmail.com>
2023-12-20 15:26:56 +0530

    install express and set port number

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import dotenv from "dotenv"
-import connectionDB from "./db/index.js"
+import connectDB from "./db/index.js"
+import {app} from "./app.js"
 dotenv.config ( {
     path:'./env'
 })
 
-connectionDB();
+connectDB().then(()=>{
+    app.listen(process.env.PORT ||8000,()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+}).catch((error)=>{
+    console.log("Mongodb connection failed",error)
+    
+})
 
 
 
